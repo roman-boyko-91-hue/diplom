362fde9ba3145424bb4c0b6b95516ef680ac1ef5
Roman <roman-boyko-91@mail.ru>
2026-05-27 21:15:59 +0500

    Исправления

--- authors/admin.py
+++ authors/admin.py
@@ -5,4 +5,4 @@ from .models import Author
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name')
-    search_fields = ('name',)
+    search_fields = ('first_name', 'last_name')
